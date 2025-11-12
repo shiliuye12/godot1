b6eccbe96932005dc2a9207738c9a4b7cb43888c
xinlinxia <1120967774@qq.com>
2025-11-12 17:27:11 +0800

    视频4

--- 4/4/Globals/ImageManager.gd
+++ 4/4/Globals/ImageManager.gd
@@ -1,11 +1,27 @@
 extends Node
 
+const FRAME_IMAGES: Array[Texture2D] = [
+	preload("uid://ddugfdjoi4qvd"),
+	preload("uid://dg7ohone2tuwp"),
+	preload("uid://frtcai03efhs"),
+	preload("uid://c4y1dihrew6aa")
+]
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var _image_list: Array[Texture2D]
 
+func _enter_tree() -> void:
+	var ifl: ImageFilesListResource = load("res://Resources/image_files_list.tres")
+	for file in ifl.file_names:
+		_image_list.append(load(file))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func get_random_item_image():
+	return _image_list.pick_random()
+
+func get_random_frame_image():
+	return FRAME_IMAGES.pick_random()
+
+func get_image(index: int):
+	return _image_list[index]
+
+func shuffle_images():
+	_image_list.shuffle()
---
+++ 4/4/Tools/ImageFiller.gd
@@ -0,0 +1,17 @@
+@tool
+extends EditorScript
+
+const PATH: String = "res://assets/glitch/"
+const RES_PATH: String = "res://Resources/image_files_list.tres"
+
+
+func _run() -> void:
+	var dir: DirAccess = DirAccess.open(PATH)
+	var ifl: ImageFilesListResource = ImageFilesListResource.new()
+	
+	if dir:
+		var files: PackedStringArray = dir.get_files()
+		for fn in files:
+			ifl.add_file(PATH + fn)
+	ResourceSaver.save(ifl, RES_PATH)
+	
